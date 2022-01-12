579f97c136a1bf80f81ed42b327852bf8e9f997f
kris <hithard5@gmail.com>
2022-01-12 19:54:18 +0100

    Fetch weather

--- source/index.js
+++ source/index.js
@@ -4,8 +4,38 @@ import './css/style.scss';
 // eslint-disable-next-line prefer-destructuring
 const API_KEY = process.env.API_KEY;
 const CITY = 'London';
-const URL = `https://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${API_KEY}`;
+const UNITS = 'metric';
+const WEATHER_URL = `https://api.openweathermap.org/data/2.5/weather?q=${CITY}&units=${UNITS}&appid=${API_KEY}`;
+// const FORECAST_URL = `api.openweathermap.org/data/2.5/forecast?q=${CITY}&appid=${API_KEY}`;
 
-fetch(URL)
-  .then((response) => response.json())
-  .then((data) => console.log(data));
+const app = document.querySelector('#app');
+const cityElem = app.querySelector('.city');
+const tempElem = app.querySelector('.temp');
+const tempUnitElem = app.querySelector('.temp-unit');
+const descElem = app.querySelector('.desc');
+const iconElem = app.querySelector('.icon');
+
+fetchWeather().then(updateWeatherUI);
+
+async function fetchWeather() {
+  const response = await fetch(WEATHER_URL);
+  const json = await response.json();
+  return json;
+}
+
+function updateWeatherUI(data) {
+  const {
+    name,
+    main: { temp },
+    weather: [primary],
+  } = data;
+
+  if (UNITS === 'metric') {
+    tempUnitElem.textContent = 'C';
+  }
+
+  cityElem.textContent = name;
+  tempElem.textContent = temp;
+  descElem.textContent = primary.description;
+  iconElem.src = `http://openweathermap.org/img/wn/${primary.icon}@2x.png`;
+}
