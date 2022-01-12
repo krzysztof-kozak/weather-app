a1b139068e0b40313922f89494f0873f87d6289d
kris <hithard5@gmail.com>
2022-01-12 17:31:47 +0100

    Test API call

--- source/index.js
+++ source/index.js
@@ -1,4 +1,11 @@
 import './css/reset.css';
 import './css/style.scss';
 
-console.log(process.env.API_KEY);
+// eslint-disable-next-line prefer-destructuring
+const API_KEY = process.env.API_KEY;
+const CITY = 'London';
+const URL = `https://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${API_KEY}`;
+
+fetch(URL)
+  .then((response) => response.json())
+  .then((data) => console.log(data));
