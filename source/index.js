87314c2e95559156bc6ce28d75e18ccd286e3b50
kris <hithard5@gmail.com>
2022-01-13 21:15:24 +0100

    Integrate search form with api call

--- source/index.js
+++ source/index.js
@@ -3,20 +3,25 @@ import './css/style.scss';
 
 // eslint-disable-next-line prefer-destructuring
 const API_KEY = process.env.API_KEY;
-const CITY = 'London';
-const UNITS = 'metric';
-const WEATHER_URL = `https://api.openweathermap.org/data/2.5/weather?q=${CITY}&units=${UNITS}&appid=${API_KEY}`;
+const DEFAULT_CITY = 'London';
+const DEFAULT_UNITS = 'metric';
 
 const app = document.querySelector('#app');
+const form = app.querySelector('form');
+const errorP = app.querySelector('.error');
+const errorSpan = app.querySelector('.error-span');
+
 const cityElem = app.querySelector('.city');
 const tempElem = app.querySelector('.temp');
 const tempUnitElem = app.querySelector('.temp-unit');
 const descElem = app.querySelector('.desc');
 const iconElem = app.querySelector('.icon');
 
-fetchWeather().then(updateWeatherUI);
+fetchWeather(DEFAULT_CITY).then(updateWeatherUI);
 
-async function fetchWeather() {
+async function fetchWeather(city) {
+  const WEATHER_URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${DEFAULT_UNITS}&appid=${API_KEY}`;
+
   const response = await fetch(WEATHER_URL);
   const json = await response.json();
   return json;
@@ -29,7 +34,7 @@ function updateWeatherUI(data) {
     weather: [primary],
   } = data;
 
-  if (UNITS === 'metric') {
+  if (DEFAULT_UNITS === 'metric') {
     tempUnitElem.textContent = 'C';
   }
 
@@ -38,3 +43,29 @@ function updateWeatherUI(data) {
   descElem.textContent = primary.description;
   iconElem.src = `http://openweathermap.org/img/wn/${primary.icon}@2x.png`;
 }
+
+function showError(city) {
+  errorSpan.textContent = city;
+  errorP.style.display = 'block';
+}
+
+function hideError() {
+  if (errorP.style.display === 'none') {
+    return;
+  }
+
+  errorP.style.display = 'none';
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  hideError();
+  const data = new FormData(form);
+  const city = data.get('city');
+
+  if (!city) {
+    return;
+  }
+
+  fetchWeather(city).then(updateWeatherUI).catch(showError.bind(null, city));
+});
